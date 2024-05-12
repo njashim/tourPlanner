21185846d069464921e5aaa328f8e333f99d096f
Nahi Islam Jashim <if23b059@technikum-wien.at>
2024-05-13 01:08:05 +0200

    added Context-File but the content in Tour and TourLog is still missing - need to fix that but later

---
+++ TourPlanner/DataAccessLayer/Entity/Context/TourPlannerContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using DataAccessLayer.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer.Entity.Context;
+
+public partial class TourPlannerContext : DbContext
+{
+    public TourPlannerContext()
+    {
+    }
+
+    public TourPlannerContext(DbContextOptions<TourPlannerContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Tour> Tours { get; set; }
+
+    public virtual DbSet<TourLog> TourLogs { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseNpgsql("Name=ConnectionStrings:TourPlannerDBConnection");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Tour>(entity =>
+        {
+            entity
+                .HasNoKey()
+                .ToTable("Tour");
+        });
+
+        modelBuilder.Entity<TourLog>(entity =>
+        {
+            entity
+                .HasNoKey()
+                .ToTable("TourLog");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ TourPlanner/DataAccessLayer/Entity/Tour.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataAccessLayer.Entity;
+
+public class Tour
+{
+}
