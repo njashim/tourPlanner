0a90e8b34d727806e96bfeb1ec9a4443be2bc450
Nahi Islam Jashim <if23b059@technikum-wien.at>
2024-05-13 02:19:11 +0200

    added crud commands for service classes but still need to fix them

---
+++ TourPlanner/BusinessLayer/Service/TourLogService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Service
+{
+    public class TourLogService
+    {
+        private readonly TourLogRepository _tourLogRepository;
+
+        public TourLogService(TourLogRepository tourLogRepository)
+        {
+            _tourLogRepository = tourLogRepository;
+        }
+
+        public async Task<TourLog> CreateTourLogAsync(TourLog newTourLog)
+        {
+            return await _tourLogRepository.CreateTourLogAsync(newTourLog);
+        }
+
+        public async Task<List<TourLog>> GetTourLogsAsync()
+        {
+            return await _tourLogRepository.GetTourLogsAsync();
+        }
+
+        public async Task<TourLog> GetTourLogByIdAsync(int tourLogId)
+        {
+            return await _tourLogRepository.GetTourLogByIdAsync(tourLogId);
+        }
+
+        public async Task<TourLog> UpdateTourLogAsync(TourLog updatedTourLog)
+        {
+            return await _tourLogRepository.UpdateTourLogAsync(updatedTourLog);
+        }
+
+        public async Task DeleteTourLogAsync(int tourLogId)
+        {
+            await _tourLogRepository.DeleteTourLogAsync(tourLogId);
+        }
+    }
+}
---
+++ TourPlanner/BusinessLayer/Service/TourService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Service
+{
+    public class TourService
+    {
+        private readonly TourRepository _tourRepository;
+
+        public TourService(TourRepository tourRepository)
+        {
+            _tourRepository = tourRepository;
+        }
+
+        public async Task<Tour> CreateTourAsync(Tour newTour)
+        {
+            return await _tourRepository.CreateTourAsync(newTour);
+        }
+
+        public async Task<List<Tour>> GetToursAsync()
+        {
+            return await _tourRepository.GetToursAsync();
+        }
+
+        public async Task<Tour> GetTourByIdAsync(int tourId)
+        {
+            return await _tourRepository.GetTourByIdAsync(tourId);
+        }
+
+        public async Task<Tour> UpdateTourAsync(Tour updatedTour)
+        {
+            return await _tourRepository.UpdateTourAsync(updatedTour);
+        }
+
+        public async Task DeleteTourAsync(int tourId)
+        {
+            await _tourRepository.DeleteTourAsync(tourId);
+        }
+    }
+}
